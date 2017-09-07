099709357a218edd593fa2e954535e8736095c8a
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-07 15:24:05 +0000

    All tests result in true and is working correctly

--- mapping.js
+++ mapping.js
@@ -3,7 +3,13 @@ var input = [
   { x: 12, y: 5 },
   { x: 8, y: 15 }
 ];
-var result = input.map(/* Your code here */);
+var result = input.map(function(obj) {
+  var objX = obj["x"]
+  var objY = obj["y"]
+  console.log(objX, objY)
+  var mathResult = Math.sqrt((objX * objX) + (objY * objY))
+  return mathResult
+});
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
